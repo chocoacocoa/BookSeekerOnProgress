6555ec2e0504052dfaaa8af453cfe6544640b806
chocoacocoa <vancholo2006@gmail.com>
2025-01-10 23:36:55 +0800

    working on POV 2, javascript

--- AR/ar.js
+++ AR/ar.js
@@ -3,12 +3,18 @@ let arrow = document.getElementById('arrow');
 let hitboxers = document.getElementById('hitbox');
 let libraryh3 = document.getElementById('libraryH3');
 let libraryH3In = document.getElementById('libraryH3In');
+let arrowLeft = document.getElementById('arrowLeft');
 function hitbox(){
     image.src = '/AR/arMedias/POVs/vertical/2.jpg';
     // arrow.style.display = 'none';
     // hitbox.style.display = 'none';
-    arrow.style.display = 'none';
-    hitboxers.style.display = 'none';
-    libraryh3.style.display = 'none';
+    // arrow.style.display = 'none';
+    // hitboxers.style.display = 'none';
+    // libraryh3.style.display = 'none';
     libraryH3In.style.display = 'block';
+    // arrowLeft.style.display = 'block';
+    arrow.style.transform = 'rotate(320deg)';
+    arrow.style.left = '17vh';
+    arrow.style.top = '65vh';
+    hitboxers.style.height = '';
 }
