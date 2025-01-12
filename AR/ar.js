1b23a049721f3b5160e3dd0747a8736e6b569f29
chocoacocoa <vancholo2006@gmail.com>
2025-01-12 20:39:00 +0800

    na ayos na so far ang arrows sa loob ng library and may glassmorphism na din sa mga back ng clickers. plus if na forgot ang code something you can say code recall sa bookseeker chat sa chatgpt account ni Cholo  and then it will say our current code, like backup code lang something.

--- AR/ar.js
+++ AR/ar.js
@@ -4,17 +4,37 @@ let hitboxers = document.getElementById('hitbox');
 let libraryh3 = document.getElementById('libraryH3');
 let libraryH3In = document.getElementById('libraryH3In');
 let arrowLeft = document.getElementById('arrowLeft');
-function hitbox(){
+let tutorialMark = document.getElementById('tutorial');
+// function hitbox(){
+//     image.src = '/AR/arMedias/POVs/vertical/2.jpg';
+//     // arrow.style.display = 'none';
+//     hitboxers.style.display = 'none';
+//     libraryh3.style.display = 'none';
+//     libraryH3In.style.display = 'block';
+//     arrowLeft.style.display = 'block';
+//     arrow.style.position = 'absolute';
+//     arrow.style.left = '18vh';
+//     arrow.style.bottom = '25vh';
+//     hitboxers.style.height = '';
+//     //     left: 18vh;
+//     // bottom: 25vh;
+//     // transform: rotate(-52deg);
+//     arrow.style.transform = 'rotate(-52deg)';
+// }
+
+function hitbox() {
     image.src = '/AR/arMedias/POVs/vertical/2.jpg';
-    // arrow.style.display = 'none';
-    // hitbox.style.display = 'none';
-    // arrow.style.display = 'none';
-    // hitboxers.style.display = 'none';
-    // libraryh3.style.display = 'none';
+    hitboxers.style.display = 'none';
+    libraryh3.style.display = 'none';
     libraryH3In.style.display = 'block';
-    // arrowLeft.style.display = 'block';
-    arrow.style.transform = 'rotate(320deg)';
-    arrow.style.left = '17vh';
-    arrow.style.top = '65vh';
+    
+    arrow.style.position = 'absolute';
+    arrow.style.left = '18vh';
+    arrow.style.bottom = '25vh';
     hitboxers.style.height = '';
+    arrow.style.transform = 'rotate(-52deg)';
+}
+
+function tutorial(){
+    alert('wag ka na magtanong kung pano\npindutin mo lang yan parang summertime saga');
 }
