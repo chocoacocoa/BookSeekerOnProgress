4dd594d011c127c5a588051364a90b8daa05bab1
chocoacocoa <vancholo2006@gmail.com>
2025-01-21 08:52:18 +0800

    pwede na ipapilot test

--- maps/two/ar.js
+++ maps/two/ar.js
@@ -20,7 +20,17 @@ let bookshelf1 = document.getElementById("bookshelf1");
 let bookshelf2 = document.getElementById("bookshelf2");
 let backLibrary = document.getElementById("backLibrary");
 let isbacklibraryrunning = false;
+let isshelf2to3running = false;
+let locationfunction;
 
+
+
+
+bookshelf1.style.opacity = '1.0';
+bookshelf2.style.opacity = '1.0';
+
+bookshelf1.style.opacity = '1.0';
+    bookshelf2.style.opacity = '0.5';
 function tutorialOnClick(){
     tutorialMat.style.display = 'block';
     tutorialdiv.style.display = 'block';
@@ -33,6 +43,36 @@ backLibrary.onclick = function () {
         alert("backbutton is disabled boss");
     }
 }
+// bookshelf1.onclick = function(){
+//     if (isshelf2to3running){
+//         s23b1();
+//     }else{
+//         alert("im depressed");
+//     }
+// }
+// function s23b1(){
+//     shelf1and2.style.fontSize = '6vmin';
+//     console.log("shelf2 and 3 clicked.");
+//     image.src = '/AR/arMedias/POVs/vertical/4.1.jpg';
+//     backHref.href = "/maps/two/ar.html";
+//     shelf6.style.display = 'none';
+//     shelf4and5.style.display = 'none';
+//     shelf2and3.style.display = 'none';
+//     shelf1and2.style.display = 'none';
+//     libraryH3In.innerHTML = 'Library > Shelf 1-2';
+//     back.style.filter = "invert(1)";
+//     bookshelf1.style.display = 'block';
+//     bookshelf2.style.display = 'block';
+//     redbook.style.display = 'none';
+//     brownbook.style.display = 'none';
+//     randombooks.style.left = 'unset';
+//     randombooks.style.top = '77vh';
+//     randombooks.style.width = '100vw';
+//     randombooks.style.borderRadius = 'unset';
+//     randombooks.style.display = 'block';
+//     bookshelf1.style.opacity = '1.0';
+//     bookshelf2.style.opacity = '0.5';
+// }
 backLibrary.style.display = 'none';
 function shelf1to2(){
     shelf1and2.style.fontSize = '6vmin';
@@ -47,6 +87,7 @@ function shelf1to2(){
     back.style.filter = "invert(1)";
 
     bookshelf1.style.display = 'block';
+    bookshelf1.style.opacity = '1.0';
     bookshelf2.style.display = 'block';
     redbook.style.display = 'block';
     brownbook.style.display = 'block';
@@ -60,21 +101,13 @@ function shelf1to2(){
     randombooks.style.top = '55vh';
     randombooks.style.width = 'fit-content';
     randombooks.style.borderRadius = '10px';
+    bookshelf1.style.opacity = '1.0';
+    bookshelf2.style.opacity = '1.0';
+    bookshelf11.onclick = function(){
+        bookshelf1();
+    }
 }
-function bookshelf11(){
-    image.src = "/AR/arMedias/POVs/vertical/3.1.1.jpg";
-    bookshelf1.style.display = 'block';
-    bookshelf2.style.display = 'block';
-    redbook.style.display = 'block';
-    brownbook.style.display = 'block';
-    randombooks.style.display = 'none';
-    brownbook.style.top = "69vh";
-    brownbook.style.left = "66vw";
-    libraryH3In.innerHTML = "Library > bookshelf 1";
-    back.style.display = "none";
-    isbacklibraryrunning = true;
-    backLibrary.style.display = 'block';
-}
+
 
 function X(){
     tutorialMat.style.display = 'none';
@@ -89,34 +122,67 @@ function shelf2to3(){
     shelf4and5.style.display = 'none';
     shelf2and3.style.display = 'none';
     shelf1and2.style.display = 'none';
-    libraryH3In.innerHTML = 'Library > Shelf 1-2';
+    libraryH3In.innerHTML = 'Library > Shelf 3';
     back.style.filter = "invert(1)";
     bookshelf1.style.display = 'block';
     bookshelf2.style.display = 'block';
-    redbook.style.display = 'block';
-    brownbook.style.display = 'block';
+    redbook.style.display = 'none';
+    brownbook.style.display = 'none';
+    randombooks.style.left = 'unset';
+    randombooks.style.top = '77vh';
+    randombooks.style.width = '100vw';
+    randombooks.style.borderRadius = 'unset';
     randombooks.style.display = 'block';
+    randombooks.innerHTML = 'random books';
     
+    // isshelf2to3running = true;
+    isbacklibraryrunning = false;
+   
+    backLibrary.style.display = 'none';
+    bookshelf2.style.opacity = '1.0';
+    bookshelf1.style.opacity = '0.5';
+    
+    bookshelf1.onclick = function (){
+        bookshelf1inshelf23();
+    }
+    bookshelf2.onclick = function () {
+        shelf2to3();
+    }
 }
-function shelf4to5onclick(){
-    shelf1and2.style.fontSize = '6vmin';
-    console.log("shelf2 and 3 clicked.");
-    image.src = '/AR/arMedias/POVs/vertical/4.1.jpg';
-    backHref.href = "/maps/two/ar.html";
-    shelf6.style.display = 'none';
-    shelf4and5.style.display = 'none';
-    shelf2and3.style.display = 'none';
-    shelf1and2.style.display = 'none';
-    libraryH3In.innerHTML = 'Library > Shelf 1-2';
-    back.style.filter = "invert(1)";
+// function shelf4to5onclick(){
+//     shelf1and2.style.fontSize = '6vmin';
+//     console.log("shelf2 and 3 clicked.");
+//     image.src = '/AR/arMedias/POVs/vertical/4.1.jpg';
+//     backHref.href = "/maps/two/ar.html";
+//     shelf6.style.display = 'none';
+//     shelf4and5.style.display = 'none';
+//     shelf2and3.style.display = 'none';
+//     shelf1and2.style.display = 'none';
+//     libraryH3In.innerHTML = 'Library > Shelf 1-2';
+//     back.style.filter = "invert(1)";
 
+//     bookshelf1.style.display = 'block';
+//     bookshelf2.style.display = 'block';
+//     redbook.style.display = 'block';
+//     brownbook.style.display = 'block';
+//     randombooks.style.display = 'block';
+// }
+function bookshelf11(){
+    image.src = "/AR/arMedias/POVs/vertical/3.1.1.jpg";
     bookshelf1.style.display = 'block';
     bookshelf2.style.display = 'block';
     redbook.style.display = 'block';
     brownbook.style.display = 'block';
-    randombooks.style.display = 'block';
+    randombooks.style.display = 'none';
+    brownbook.style.top = "65vh";
+    brownbook.style.left = "64vw";
+    libraryH3In.innerHTML = "Library > bookshelf 1";
+    back.style.display = "none";
+    isbacklibraryrunning = true;
+    backLibrary.style.display = 'block';
+    bookshelf1.style.opacity = '1.0';
+    bookshelf2.style.opacity = '0.5';
 }
-
 function bookshelf22(){
     image.src = '/maps/two/arMedias/POVs/vertical/3.1.2.jpg';
     redbook.style.display = 'none';
@@ -130,6 +196,68 @@ function bookshelf22(){
     randombooks.style.width = '100vw';
     randombooks.style.borderRadius = 'unset';
     randombooks.style.display = 'block';
-    
+    bookshelf2.style.opacity = '1.0';
+    bookshelf1.style.opacity = '0.5';
+    bookshelf11.onclick = function(){
+        bookshelf1inshelf23();
+    }
+}
+function bookshelf1inshelf23(){
+    image.src = '/maps/two/arMedias/POVs/vertical/bookshelf2.jpg';
+    redbook.style.display = 'none';
+    brownbook.style.display = 'none';
+    isbacklibraryrunning = false;
+    backLibrary.style.display = 'block';
+    back.style.display = 'none';
+    libraryH3In.innerHTML = 'Library > Shelf 2';
+    randombooks.style.left = 'unset';
+    randombooks.style.top = '77vh';
+    randombooks.style.width = '100vw';
+    randombooks.style.borderRadius = 'unset';
+    randombooks.style.display = 'block';
+    randombooks.innerHTML = 'Map of the Philippines';
+    isbacklibraryrunning = false;
+    backLibrary.style.display = 'none';
+    back.style.display = 'block';
+    bookshelf1.style.opacity = '1.0';
+    bookshelf2.style.opacity = '0.5';
+}
 
+
+
+function shelf4to5(){
+    shelf1and2.style.fontSize = '6vmin';
+    console.log("shelf2 and 3 clicked.");
+    image.src = '/AR/arMedias/POVs/vertical/4.1.jpg';
+    backHref.href = "/maps/two/ar.html";
+    shelf6.style.display = 'none';
+    shelf4and5.style.display = 'none';
+    shelf2and3.style.display = 'none';
+    shelf1and2.style.display = 'none';
+    libraryH3In.innerHTML = 'Library > Shelf 3';
+    back.style.filter = "invert(1)";
+    bookshelf1.style.display = 'block';
+    bookshelf2.style.display = 'block';
+    redbook.style.display = 'none';
+    brownbook.style.display = 'none';
+    randombooks.style.left = 'unset';
+    randombooks.style.top = '77vh';
+    randombooks.style.width = '100vw';
+    randombooks.style.borderRadius = 'unset';
+    randombooks.style.display = 'block';
+    randombooks.innerHTML = 'random books';
+    
+    // isshelf2to3running = true;
+    isbacklibraryrunning = false;
+   
+    backLibrary.style.display = 'none';
+    bookshelf2.style.opacity = '1.0';
+    bookshelf1.style.opacity = '0.5';
+    
+    bookshelf1.onclick = function (){
+        bookshelf1inshelf23();
+    }
+    bookshelf2.onclick = function () {
+        shelf2to3();
+    }
 }
